c534030b78ae13d181720dee51c46aaddedd99cd
Michael Posa <posa@seas.upenn.edu>
2022-01-29 16:05:57 -0500

    Add more complete MultibodyPlant demo

---
+++ symbolic_plant_test.py
@@ -0,0 +1,139 @@
+import pydrake.symbolic as sym
+import torch
+import symbolic
+import numpy as np
+
+
+from pydrake.common import FindResourceOrThrow
+from pydrake.multibody.parsing import Parser
+from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
+from pydrake.systems.analysis import Simulator
+from pydrake.systems.framework import DiagramBuilder
+from pydrake.multibody.tree import JacobianWrtVariable
+
+from pydrake.all import (MultibodyForces_, Expression, RotationalInertia_, SpatialInertia_, UnitInertia_)
+
+#
+# Build the symbolic plant and context
+#
+
+builder = DiagramBuilder()
+plant, _ = AddMultibodyPlantSceneGraph(builder, 0.0)
+Parser(plant).AddModelFromFile(
+    FindResourceOrThrow("drake/examples/pendulum/Pendulum.urdf"))
+plant.Finalize()
+sym_plant = plant.ToSymbolic()
+context = sym_plant.CreateDefaultContext()
+
+#
+# symbolic variables
+#
+
+# position of the joint w.r.t. the base
+r_joint = sym.MakeVectorVariable(3, 'r_joint')
+
+# position of a point of interest on the arm body
+r = sym.MakeVectorVariable(3, 'r')
+
+# mass and inertia of the arm link
+m = sym.Variable('m')
+
+# NOTE: I is scaled by the mass, so the actual moment of inertia is m*I
+# This is moment of inertia about the link origin (joint), not COM
+I = sym.MakeVectorVariable(3, 'I')
+
+# pendulum state
+q = sym.MakeVectorVariable(sym_plant.num_positions(), 'q')
+v = sym.MakeVectorVariable(sym_plant.num_velocities(), 'v')
+
+#
+# Pytorch copies of symbolic variables
+#
+r_joint_pt = torch.tensor([-1, .5, 2])
+r_pt = torch.tensor([3.0, 4.0, 5.0])
+m_pt = torch.tensor([1])
+I_pt = torch.tensor([.1, .2, .2])
+
+q_pt = torch.tensor([1.0])
+v_pt = torch.tensor([2.0])
+
+
+
+#
+# Set mass, inertia, and position variables
+#
+arm = sym_plant.GetBodyByName("arm")
+arm.SetMass(context, m)
+
+inertia = arm.CalcSpatialInertiaInBodyFrame(context)
+# I_SScm_E = RotationalInertia_[Expression](I[0], I[1], I[2])
+# inertia_sym = SpatialInertia_[Expression].MakeFromCentralInertia(1, inertia.get_com(), I_SScm_E)
+# arm.SetSpatialInertiaInBodyFrame(context, inertia_sym)
+
+sym_inertia = SpatialInertia_[Expression](m, inertia.get_com(),
+    UnitInertia_[Expression](I[0], I[1], I[2]))
+arm.SetSpatialInertiaInBodyFrame(context, sym_inertia)
+
+#
+# Get the joint named "theta" and change its position w.r.t. the parent
+#
+joint=sym_plant.GetJointByName("theta")
+joint_frame_on_parent = joint.frame_on_parent()
+X_parent_joint = joint_frame_on_parent.GetFixedPoseInBodyFrame()
+X_parent_joint.set_translation(r_joint)
+joint_frame_on_parent.SetPoseInBodyFrame(context, X_parent_joint)
+
+
+#
+# Set the symbolic state
+#
+sym_plant.SetPositionsAndVelocities(context, np.array([q,v]))
+
+#
+# Calculate the position of point r on the frame
+#
+
+world_frame = sym_plant.world_frame()
+pos = sym_plant.CalcPointsPositions(
+    context=context, frame_B=arm.body_frame(),
+    p_BQi=r, frame_A=world_frame)
+
+#
+# Calculate the jacobian of point r on the arm
+#
+wrt = JacobianWrtVariable.kV
+J = sym_plant.CalcJacobianTranslationalVelocity(
+    context=context, with_respect_to=wrt, frame_B=arm.body_frame(),
+    p_BoBi_B=r, frame_A=world_frame, frame_E=world_frame)
+
+#
+# Calculate the mass matrix
+#
+M = sym_plant.CalcMassMatrixViaInverseDynamics(context)
+
+# 
+# Example: convert J to a function of (q,v,r,r,r_joint)
+#
+[func_J, string_J] = symbolic.sym_to_pytorch(J, q, v, r, r_joint)
+J_pt = func_J(q_pt, v_pt, r_pt, r_joint_pt)
+
+#
+# Example: convert pos to a function with a single argument, stacked
+#   [q;r;r_joint]
+#
+[func_pos, string_pos] = symbolic.sym_to_pytorch(pos, np.hstack((q, r, r_joint)))
+func_pos(torch.hstack((q_pt, r_pt, r_joint_pt)))
+
+forces = MultibodyForces_[Expression](sym_plant)
+sym_plant.CalcForceElementsContribution(context, forces)
+
+
+#
+# Example: convert M to a function with multiple arguments (q, r_joint, m, I)
+#
+[func_M, string_M] = symbolic.sym_to_pytorch(M, q, r_joint, m, I)
+M_pt = func_M(q_pt, r_joint_pt, m_pt, I_pt)
+
+
+# import pdb
+# pdb.set_trace()
--- test_pytorch.py
+++ test_pytorch.py
@@ -1,4 +1,3 @@
-# import pdb; pdb.set_trace()
 import pydrake.symbolic as sym
 import torch
 import symbolic
@@ -23,71 +22,3 @@ print(func(tx))
 [func, string] = symbolic.sym_to_pytorch(expr_list, x)
 print(string)
 print(func(tx))
-
-
-from pydrake.common import FindResourceOrThrow
-from pydrake.multibody.parsing import Parser
-from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
-from pydrake.systems.analysis import Simulator
-from pydrake.systems.framework import DiagramBuilder
-from pydrake.multibody.tree import JacobianWrtVariable
-
-builder = DiagramBuilder()
-plant, _ = AddMultibodyPlantSceneGraph(builder, 0.0)
-Parser(plant).AddModelFromFile(
-    FindResourceOrThrow("drake/examples/pendulum/Pendulum.urdf"))
-plant.Finalize()
-
-plant_sym = plant.ToSymbolic()
-
-context = plant_sym.CreateDefaultContext()
-r = sym.MakeVectorVariable(3, 'r')
-q = sym.MakeVectorVariable(plant_sym.num_positions(), 'q')
-v = sym.MakeVectorVariable(plant_sym.num_velocities(), 'v')
-plant_sym.SetPositionsAndVelocities(context, np.array([q,v]))
-# r = np.array([1,2,3]).T
-
-print(r)
-
-
-wrt = JacobianWrtVariable.kV
-world_frame = plant_sym.world_frame()
-frame = plant_sym.GetFrameByName("arm")
-J = plant_sym.CalcJacobianTranslationalVelocity(
-    context=context, with_respect_to=wrt, frame_B=frame,
-    p_BoBi_B=r, frame_A=world_frame, frame_E=world_frame)
-
-print(J)
-
-print(type(J))
-
-[func, string] = symbolic.sym_to_pytorch(J, q, v, r)
-
-print(string)
-
-print(plant_sym.GetPositionsAndVelocities(context))
-
-qt = torch.tensor([1.0])
-vt = torch.tensor([2.0])
-rt = torch.tensor([3.0, 4.0, 5.0])
-
-print(func(qt, vt, rt))
-
-
-# Check accuracy of the expression
-[func, string] = symbolic.sym_to_pytorch(expr_all, x, y)
-result_torch = func(tx, ty)
-
-
-sym_stack = np.hstack((x,y))
-value_stack = np.hstack((tx.detach().numpy(), ty.detach().numpy()))
-value_vectorized = np.vectorize(sym.Expression)
-value_stack_expr = value_vectorized(value_stack)
-
-subs_dict = dict(zip(sym_stack, value_stack_expr))
-[_, value_1] = expr_1.Substitute(subs_dict).Unapply()
-[_, value_2] = expr_2.Substitute(subs_dict).Unapply()
-[_, value_3] = expr_3.Substitute(subs_dict).Unapply()
-
-error = np.hstack((value_1, value_2, value_3)) - result_torch.detach().numpy()
-print(error)
