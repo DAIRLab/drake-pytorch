3ae4a7016e5d632b3f9e3a97dcc9d6b8b812b75f
Mathew Halm <mshalm95@gmail.com>
2022-01-31 11:41:02 -0500

    Crude macOS setup which prefers preinstalled drake

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
+install_reqs = ['torch']
+try:
+    import pydrake
+    print('USING FOUND DRAKE VERSION')
+except ModuleNotFoundError as e:
+    install_reqs += ['drake']
 
 setup(
     name='drake-pytorch',
     version='0.1',
     packages=['drake_pytorch',],
-    install_requires=[
-        'drake',
-        'torch'
-    ]
+    install_requires=install_reqs
 )
